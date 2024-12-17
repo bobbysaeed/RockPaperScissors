10bfa0d39e848d3f35e173eea8f74ecd3fe57074
saeed <saeedbabakan10@gmail.com>
2024-12-17 11:38:37 -0800

    add user choice functionality

--- src/main.py
+++ src/main.py
@@ -2,3 +2,10 @@ class RockPaperScissors():
 
     def __init__(self):
         self.choices = ['rock', 'paper', 'scissors']
+
+    def get_user_choice(self):
+        user_choice = input(f"Please enter your choice from {self.choices}")
+        if user_choice.lower() in self.choices:
+            return user_choice.lower()
+        print("please enter a valid choice")
+        return self.get_user_choice()
