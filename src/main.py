78728b64875c3104bdeea128f3273adf672acf9e
saeed <saeedbabakan10@gmail.com>
2024-12-18 00:47:18 -0800

    add deciding the winner reason

--- src/main.py
+++ src/main.py
@@ -12,3 +12,15 @@ class RockPaperScissors():
 
     def get_pc_choice(self):
         return random.choice(self.choices)
+
+    def get_the_winner(self, user_choice, pc_choice):
+        winning_Conditions = [('rock', 'scissors'), ('paper', 'rock'), ('scissors', 'paper')]
+
+        if user_choice == pc_choice:
+            print("it's a tie!")
+        for winning_Condition in winning_Conditions:
+
+            if winning_Condition[1] == pc_choice and winning_Condition[0] == user_choice:
+                print("You Have Won! CONGRATS...")
+
+        print("Oops! You just lost to a Computer")
